2550d5bafb87d73c377d6fd97ec226d0ad7fbf12
Ryan Goodwin <ryangoodwin0818@gmail.com>
2026-01-15 14:36:57 -0600

    add envs

--- include/environments/environments.h
+++ include/environments/environments.h
@@ -1,10 +1,39 @@
 #pragma once
 #include "node.h"
 
-// NOTE: This is just for defining what the actual environments need to have for static polymorphism.
-// NOTE: This class will never actually be used, and should be deleted once the actual environments are implemented.
+template<typename StateType> class GridEnvironment { 
+// template may not be necessary for static polymorphism and inlining
+public:
 
-template<typename StateType> class Environment {
+  void get_successors(uint32_t node_index, std::vector<uint32_t>& neighbors);
+  uint32_t get_heuristic(unint32_t node_index);
+  bool is_goal(uint32_t node_index);
+
+  uint32_t get_start_node();
+
+  NodePool& pool;
+
+  std::vector<StateType> node_states; // node_index -> StateType
+
+};
+
+template<typename StateType> class PancakeEnvironment {
+
+public:
+
+  void get_successors(uint32_t node_index, std::vector<uint32_t>& neighbors);
+  uint32_t get_heuristic(unint32_t node_index);
+  bool is_goal(uint32_t node_index);
+
+  uint32_t get_start_node();
+
+  NodePool& pool;
+
+  std::vector<StateType> node_states; // node_index -> StateType
+
+};
+
+template<typename StateType> class SlidingTileEnvironment {
 
 public:
 
